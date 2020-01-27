a7e7fb34ece5b86da269afec6a572b5e3e7dfa5f
plaetzaw <plaetzaw@gmail.com>
2020-01-26 19:07:24 -0600

    70% fixing Crit Error and Scouter Error

--- hero_rpg.py
+++ hero_rpg.py
@@ -7,15 +7,15 @@
 # 3. flee
 
 import random
-import time
+from time import sleep
 
 class Character:
-    def __init__(self, health, power, name, coins):
+    def __init__(self, health, power, name, coins, armor):
         self.health = health
         self.power = power
         self.name = name
         self.coins = coins
-        self.armor = 0
+        self.armor = armor
 
     def attack(self, othercharater): #module
         othercharater.health -= self.power
@@ -39,12 +39,22 @@ class Hero(Character):
         othercharater.health -= self.damage
         print(f"{self.name} has dealt {self.damage} to {othercharater.name}")
         if othercharater.health <= 0:
-            print(f"{othercharater.name} has been slain!") 
+            print(f"{othercharater.name} has been slain!")
+            self.coins += othercharater.coins
+            print(f"You loot through the body of {othercharater.name} and scavange up {othercharater.coins} coins from their dead corpse")
+    
+    def alive(self):
+        while self.health > 0:
+            return True
+        else:
+            sleep(2)
+            print("You have beeen slain!")
+            return False
 
     def crit(self): ###Crit Bug Is somewhere around here###
         self.critDamage = 1
         critChance = random.randint(1, 5)
-        if critChance == 5:
+        if critChance == 2:
             print(f'{self.name} has landed a critical blow against!')
             self.critDamage = self.power * 2
             return self.critDamage
@@ -53,8 +63,11 @@ class Hero(Character):
             return self.critDamage
 
     def buy(self, item):
-        self.coins -= item.cost
-        item.apply(self)
+        if self.coins >= item.cost:
+            self.coins -= item.cost
+            item.apply(self)
+        else:
+            print("You don't have enough coins to purchase this item!")
 
 class Goblin(Character):
     pass
@@ -66,7 +79,7 @@ class Zombie(Character):
 class Medic(Character):
     def heal(self):
         healChance = random.randint(1, 5)
-        if healChance != 5:
+        if healChance == 5:
             self.health += 2
             print('Blessed by RNGsus, the Medic has healed itself by 2!')
 
@@ -84,11 +97,15 @@ class Shade(Character):
 
 class Barbarian(Character):
     def attack(self, othercharater):
-        self.power = random.randint(5, 13)
+        self.power = random.randint(9, 22)
         othercharater.health -= self.power
         print("{} do {} damage to the {}.".format(self.name, self.power, othercharater.name))
         if othercharater.health <= 0:
             print("{} has been slain!".format(othercharater.name))
+
+class TheBridgekeeper(Character):
+    pass
+    
 
 class SuperTonic:
     def __init__(self):
@@ -112,43 +129,62 @@ class Longsword:
         self.name = "Longsword"
     def apply(self, character):
         character.power += 5
-        print("Your character's power has increased by 5!")
+        print("No longer forced to use their fists {self.name} swings a long, sharp sword!")
+        sleep(1)
+        print("You will deal significantly more damage now!")
 
 class SSJ:
     def __init__(self):
         self.cost = 30
         self.name = "SuperSayin"
     def apply(self, character):
-        character.power *= 1.75
+        character.power *= 3.5
         print("Your character lets out a primal roar, their hair pulsates and turns into golden. You are far more powerful now")
+
+class Scouter:
+    def __init__(self):
+        self.cost = 3
+        self.name = "Scouter-Glass"
+    def apply(self, character): #Enabling the scouter doesn't work
+        displayStatus == True
+        print("Your character can now view the power and health of themselves and your foe")
 
 class Store:
     tonic = SuperTonic()
     armor = Armor()
     longsword = Longsword()
     supersayin = SSJ()
-    items = [tonic, armor, longsword, supersayin]
+    scouter = Scouter()
+    items = [tonic, armor, longsword, supersayin, scouter]
     def do_shopping(self, PC):
-        while True:
-            print("=====================")
-            print("Welcome to the store!")
-            print("=====================")
-            print("You have {} coins.".format(PC.coins))
+        sleep(1)
+        print("You walk forward, a small hut stands before you\n")
+        sleep(2)
+        print("A small hermit pokes his head out and speaks to you\n")
+        sleep(2)
+        print("Greetings Noble Spiderman! I have wares, if you have coin!\n")
+        sleep(2)
+        while True:    
             print("What do you want to do?")
+            print("You have {} coins.".format(PC.coins))
             for i in range(len(self.items)):
                 item = self.items[i]
                 print("{}. buy {} ({})".format(i + 1, item.name, item.cost))
-            print("10. leave")
+            print("0. leave")
             raw_imp = int(input("> "))
-            if raw_imp == 10:
+            if raw_imp == 0:
                 break
             else:
                 ItemToBuy = Store.items[raw_imp - 1]
                 item = ItemToBuy
                 PC.buy(item)
     def go_shopping(self, character):
-        store_status = int(input("""1. Enter the store. 
-    Press another number to battle."""))
+        print("You see a smoketrail off over the treeline\n")
+        sleep(1)
+        print("Will you approach it?\n")
+        sleep(1)
+        print("Press 1 to approach, to remain in the forest press 2")
+        store_status = int(input())
         if store_status == 1:
             self.do_shopping(character)
 
@@ -179,32 +215,45 @@ class Store:
 #             print("You have been slain!")
 #             return False        
 
-spiderman = Hero(100, 6, "Spiderman the Great", 5)
-goblinman = Goblin(20, 5, "Goblinman the Foul", 3)
-zombieman = Zombie(0, 7, "Zombieman the Invincible", 1000)
-effinghealer = Medic(30, 6, "Leeroy Jenkins the NotMedic", 11)
-shademan = Shade(1, 6, "Sylvannas the Tormented", 14)
-barbyman = Barbarian(42, 0, "Barbarossa the Forsaken", 22)
+spiderman = Hero(100, 12, "Spiderman the Great", 5, 0)
+goblinman = Goblin(20, 7, "Goblinman the Foul", 8, 0)
+zombieman = Zombie(0, 7, "Zombieman the Invincible", 1000, 4)
+effinghealer = Medic(40, 6, "Leeroy Jenkins the NotMedic", 11, 3)
+shademan = Shade(1, 6, "Sylvannas the Tormented", 14, 0)
+barbyman = Barbarian(60, 0, "Barbarossa the Forsaken", 22, 2)
+bridgeboi = TheBridgekeeper(1000000, 9001, "The Legendary Bridgekeeper of Kazadum", 9000, 9000)
 store = Store()
 
+displayStatus = False #This is the variable to enable the scouter
 
 def main():
-    while spiderman.alive() and goblinman.alive():
+    print("You stand alone, in the middle of a lightly dense forest of pine trees\n")
+    sleep(1)
+    print("A tree in front of you rustles and a small goblin bursts into the clearling\n")
+    sleep(1)
+    print("{} appraoches you".format(goblinman.name))
+    if displayStatus == True:
         print(spiderman.status())
         print(goblinman.status())
-        print("...before you {} appraoches...".format(goblinman.name))
+        sleep(1)
+    else:
+        pass
+    while spiderman.alive() and goblinman.alive():
         print("What do you want to do?")
-        print("1. fight enemy")
-        print("2. do nothing")
-        print("3. flee")
+        print("1. Fight {}".format(goblinman.name))
+        print("2. Do Nothing")
+        print("3. Flee from {}".format(goblinman.name))
         print("> ", end=' ')
         raw_input = input()
-        if raw_input == "1":
+        if raw_input == "1": 
             spiderman.attack(goblinman)
         elif raw_input == "2":
             pass
         elif raw_input == "3":
-            print("Goodbye.")
+            print("You flee from {}.".format(goblinman.name))
+            sleep(1)
+            print("Wow, seriously? You fled from the first enemy you encountered?")
+            sleep(1)
             break
         else:
             print("Invalid input {}".format(raw_input))
@@ -215,15 +264,91 @@ def main():
 
     store.go_shopping(spiderman)
 
-
-    while spiderman.alive() and effinghealer.alive():
+    print("...before you {} appraoches...".format(zombieman.name))
+    sleep(1)
+    print("You sense that this is a foe that you cannot defeat by conventional means")
+    if displayStatus == True:
         print(spiderman.status())
-        print(effinghealer.status())
-        print()
+        print(goblinman.status())
+        sleep(1)
+    else:
+        pass
+    sleep(1)
+    while spiderman.alive() and zombieman.alive():    
         print("What do you want to do?")
-        print("1. fight enemy")
-        print("2. do nothing")
-        print("3. flee")
+        print("1. Fight {}".format(zombieman.name))
+        print("2. Do nothing")
+        print("3. Flee from {}".format(zombieman.name))
+        print("4. Inspect {} more closely".format(zombieman.name))
+        print("> ", end=' ')
+        raw_input = input()
+        if raw_input == "1":
+            spiderman.attack(zombieman)
+        elif raw_input == "2":
+            pass
+            print("{} stares blankly at you, but opts not to attack".format(zombieman.name))
+        elif raw_input == "3":
+            print("You flee from {}.".format(zombieman.name))
+            break
+        elif raw_input == "4":
+            sleep(1)
+            print("You inspect the rooting corpse of {}\n".format(zombieman.name))
+            sleep(1)
+            print("Upon its corpse you see a strange glyph\n")
+            sleep(1)
+            print("You can't get a good look at it as {}\n".format(zombieman.name))
+            sleep(1)
+            print("continues to lunge around and amble aimlessly\n")
+            sleep(1)
+            print("Perhaps if you knocked it down, you could get a better look at it?\n")
+            sleep(1)
+            print("Will you attempt to grapple {} to the ground?\n".format(zombieman.name))
+            print("1. = Yes or 2 = No")
+            raw_input = input()
+            if raw_input == "1":
+                grap_check = random.randint(1,5)
+                if grap_check >= 3:
+                    sleep(1)
+                    print("Success! You knock {} to the ground".format(zombieman.name))
+                    sleep(1)
+                    print("You get a better look at the glyph- it bears the markings of a phoenix\n")
+                    sleep(1)
+                    print("This could be valuable information, but you're not yet sure why\n")
+                    sleep(1)
+                    print("You decide there is no reason to linger around this foul thing anymore")
+                    spiderman.coins += zombieman.coins
+                    print("But not before relieving {} of his {} coins before he can get back to his feet".format(zombieman.name, zombieman.coins))
+                    break
+                elif grap_check < 3:
+                    sleep(1)
+                    print("Failure! {} overpowers you and swipes at you".format(zombieman.name))
+            elif raw_input == "2":
+                break        
+
+
+        else:
+            print("Invalid input {}".format(raw_input))
+
+            # zombieman.health >= 0:      
+        if spiderman.attack(zombieman):
+            zombieman.attack(spiderman)
+
+    store.go_shopping(spiderman)
+
+    print("A man cladded in an oversized robe approaches you\n")
+    sleep(1)
+    print("My name is {}, and I have no quarrel with you\n")
+    sleep(1)
+    print("That said, I will defend myself should you attack me\n")
+    print(spiderman.status())
+    sleep(0.5)
+    print(effinghealer.status())
+    sleep(0.5)
+    while spiderman.alive() and effinghealer.alive():
+        print("What do you want to do?")
+        print("1. Fight {}".format(effinghealer.name))
+        print("2. Do Nothing")
+        print("3. Flee from {}".format(effinghealer.name))
         print("> ", end=' ')
         raw_input = input()
         if raw_input == "1":
@@ -231,20 +356,21 @@ def main():
         elif raw_input == "2":
             pass
         elif raw_input == "3":
-            print("Goodbye.")
+            print("You flee from {}.".format(effinghealer.name))
             break
         else:
             print("Invalid input {}".format(raw_input))
 
         if effinghealer.health > 0:
             effinghealer.attack(spiderman)
+            effinghealer.heal()
 
     store.go_shopping(spiderman)
 
-    while spiderman.alive() and shademan.alive():
-        print(spiderman.status())
-        print(shademan.status())
-        print()
+    print(spiderman.status())
+    print(shademan.status())
+    sleep(1)
+    while spiderman.alive() and shademan.alive():    
         print("What do you want to do?")
         print("1. fight enemy")
         print("2. do nothing")
@@ -252,7 +378,13 @@ def main():
         print("> ", end=' ')
         raw_input = input()
         if raw_input == "1":
-            spiderman.attack(shademan)
+            dodgeChance = random.randint(1, 10)
+            if dodgeChance == 10:
+                spiderman.health -= shademan.power
+                spiderman.attack(shademan)
+            else: print("{} swung and {} nimbly dodged the strike!".format(shademan.name, spiderman.name))
+
+
         elif raw_input == "2":
             pass
         elif raw_input == "3":
@@ -281,8 +413,12 @@ def main():
         elif raw_input == "2":
             pass
         elif raw_input == "3":
-            print("Goodbye.")
-            break
+            sleep(1)
+            print("As you turn to flee {} catches you with his axe and slams you into the ground.".format(barbyman.name))
+            barbyman.attack(spiderman)
+            sleep(1)
+            print("You cannot flee from this foe!")
+            
         else:
             print("Invalid input {}".format(raw_input))
 
@@ -291,5 +427,63 @@ def main():
 
     store.go_shopping(spiderman)
 
+    while spiderman.alive() and bridgeboi.alive():
+            print(spiderman.status())
+            print(bridgeboi.status())
+            print()
+            print("What do you want to do?")
+            print("1. fight enemy")
+            print("2. do nothing")
+            print("3. flee")
+            print("> ", end=' ')
+            raw_input = input()
+            if raw_input == "1":
+                print("You intend to fight me? You stand no chance against my power, however I will give you a chance to live if you can answer my riddles\n")
+                sleep(1)
+                print("If you will fight press 1, if you will test your wits press 2, press anything else and you will be deleted\n")
+                raw_input = input()
+                if raw_input == "2":
+                    sleep(1)
+                    print("The bridgekeeper cackles\n")
+                    sleep(1)
+                    print("I see you are not as dumb as you look, very well\n")
+                    sleep(1)
+                    print("Question 1! Who was the first enemy you fought?\n")
+                    sleep(1)
+                    print("1. Goblinman the Foul")
+                    print("2. Gobliman the Wretched")
+                    print("3. Goblianman the Complacent")
+                    raw_input = input()
+                    if raw_input == "1":
+                        sleep(1)
+                        print("You are correct, next question...\n")
+                        sleep(1)
+                        print("Question 2! What glpyh did {} bear?\n".format(zombieman.name))
+                        print("1. A deer")
+                        print("2. A phoenix")
+                        print("3. A snake")
+                        raw_input = input()
+                        if raw_input == "2":
+                            sleep(1)
+                            print("You are correct, final question...1")
+                        else:
+                            print("WRONG!")
+                            bridgeboi.attack(spiderman)
+                    else:
+                        print("WRONG!")
+                        bridgeboi.attack(spiderman) 
+                else:
+                    bridgeboi.attack(spiderman)    
+
+            elif raw_input == "2":
+                pass
+            elif raw_input == "3":
+                print("Goodbye.")
+                break
+            else:
+                print("Invalid input {}".format(raw_input))
+
+            if bridgeboi.health > 0:
+                bridgeboi.attack(spiderman)    
 
 main()
